6370216bcf7f24ed8ce6f048ec2351560e2967d0
kellyhaps <58571533+kellyhaps@users.noreply.github.com>
2024-12-10 21:55:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,85 @@
 import streamlit as st
 import random
+import json
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+#To do for highscore;
+#Function to check if highscore
+#function to get the name
+#At score to list
+#sort list low to high and drop number 5
 
 #page setup
 st.set_page_config(page_title="GuesGame",page_icon="🌟")
 
 #title
-st.title("Gues the number2")
+st.title("Gues the number2.2")
 st.subheader("In how many tries can you guess the number? :sunglasses:")
 
+#open highscore
+with open ('highscore.json') as f:
+	highscore_list = json.load(f)
+
+#enter high score to list
+def enter_high_score():
+	#open highscore
+	with open ('highscore.json') as f:
+		highscore_list = json.load(f)
+	#get time
+	now = str(datetime.now())
+	now = now[:19]
+	#get name & guessen
+	name_win = st.session_state.name_win
+	gueses = st.session_state.gueses_win
+	#create win dictonary
+	win_dict={"Name":name_win,"score":gueses,"Date":now}
+	highscore_list.append(win_dict)
+	#save list;
+	with open ('highscore.json', 'w') as f:
+		json.dump(highscore_list,f)
+	#set button to fals
+	#st.session_state.enter_high_score = False
+	del st.session_state.open_dialog
+
+print("dialog")
+#dialog box for winning state
+#if "open_dialog" in st.session_state:
+@st.dialog("You got a highscore!")
+def win_name():
+	st.session_state.name_win = st.text_input("What's your name?")
+	win_btn = st.button("Enter highscore",on_click=enter_high_score)
+	#st.rerun()
+
+
+#check for highscore
+def check_for_higscore():
+	#get guesscore;
+	gueses = st.session_state.gues_count
+	print(f"The gueses = {gueses}")
+	#check for highscore
+	for x in highscore_list:
+		print(f"Score is; {x["score"]}")
+		if gueses < x["score"]:
+			#Highscoor balloons;
+			st.balloons()
+			print("winning here")
+			#open name box
+			st.session_state.open_dialog = "Hoi"
+			st.session_state.gueses_win = st.session_state.gues_count
+			#	name_win = st.text_input("What's your name?")
+			#@st.dialog("You got a highscore!")
+			#def win_name():
+			#	st.session_state.name_win = st.text_input("What's your name?")
+			#	win_btn = st.button("Enter highscore",on_click=enter_high_score)
+			win_name()
+
+			
+			break
+
+
+#Create a random number
 def random_number2():
 	st.session_state.random_number = random.randint(0,100)
 	st.session_state.gues_count = 0
@@ -27,7 +99,7 @@ placeholder = st.empty()
 #check if number is create;
 if "random_number" in st.session_state:
 	placeholder.write(f"Random number picked")
-	#print(st.session_state.random_number)
+	print(st.session_state.random_number)
 
 #button
 st.button("Pick a number",on_click=random_number2, icon=":material/refresh:")
@@ -42,11 +114,14 @@ if "win" in st.session_state:
 	#Check if you won
 	if win == 0:
 		placeholder2.subheader(f"You got it!! It took you; {st.session_state.gues_count}")
-		st.balloons()
+		
+		#function to check for win
+		check_for_higscore()
 		#set gues count to 0
 		random_number2()
-		#st.session_state.gues_count = 0
-		#st.session_state.win = 1
+		#rerun app.
+		#st.rerun()
+
 	#To high
 	if win > 0:
 		placeholder2.write(f"You guesed to high! - {st.session_state.gues_count}")
@@ -59,3 +134,36 @@ else:
 	st.session_state.gues_count = 0
 #button to gues the number
 st.button("gues the number", on_click=gues_number)
+
+# --- section for highscore ---------------------------------------
+st.divider()
+
+#sort the highscore;
+highscore_list = sorted(highscore_list, key=lambda x: x['score'])
+
+#At number to highscore;
+highscore_list_2=[]
+count = 1
+for x in highscore_list:
+	#x = {"Place":score}
+	x["Place"] = count
+	highscore_list_2.append(x)
+	count+=1
+	#stop for top 5;
+	if count > 5:
+		#save this new 5 number long list
+		with open ('highscore.json', 'w') as f:
+			json.dump(highscore_list_2,f)
+		break
+
+
+
+#print as a table
+df = pd.DataFrame(highscore_list_2)
+
+# Move the last column to the first position
+columns = df.columns.tolist()
+columns = [columns[-1]] + columns[:-1]
+df = df[columns]
+#Put it to the screen;
+st.markdown(df.to_html(index=False), unsafe_allow_html=True)
